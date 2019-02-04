bb028634b868516feaeea165a96ee4ad64fe178d
Elliot Snethen <elliot.snethen@gmail.com>
2019-02-04 07:53:03 -0800

    Added Main.py

---
+++ Main.py
@@ -0,0 +1,10 @@
+import pygame
+from pygame.locals import *
+pygame.init()
+import random
+import time
+import sys
+
+screen = pygame.display.set_mode([1200 , 800])
+pygame.mouse.set_visible(False)
+font = pygame.font.SysFont("comicsansms", 20)
