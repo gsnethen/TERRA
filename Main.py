34d0ef5b5c825bc0b67b558fb3d45c11770696d5
Elliot Snethen <elliot.snethen@gmail.com>
2019-02-04 11:47:53 -0800

    You can move and collide now

--- Main.py
+++ Main.py
@@ -12,8 +12,9 @@ pygame.mouse.set_visible(False)
 
 world = WorldGeneration.generateWorld()
 
-playerPos = [1200, 800]
-cameraOffset = [622.5, 422.5]
+playerPos = [0, 0]
+cameraOffset = [-622.5, -422.5]
+playerVel = [0, 0]
 
 tileImages = {"TILE_AIR" : pygame.image.load("AirImage.png"),
               "TILE_DIRT" : pygame.image.load("DirtImage.png"),
@@ -38,15 +39,40 @@ while True:
             sys.exit()
 
     keys = pygame.key.get_pressed()
+
+    tickTime = time.time() - tickTime
+
+    playerVel[0] -= (playerVel[0] * 3) * tickTime
+    playerVel[1] += 665 * tickTime
+
+    playerPos[0] += playerVel[0] * tickTime
+    cameraOffset[0] += playerVel[0] * tickTime
+    for row in world:
+            for tile in row:
+                if tile[0] != "TILE_AIR":
+                    if Rect(playerPos[0], playerPos[1], 45, 45).colliderect(Rect(tile[1][0] * 45, tile[1][1] * 45, 45, 45)):
+                        playerPos[0] -= playerVel[0] * tickTime
+                        cameraOffset[0] -= playerVel[0] * tickTime
+                        playerVel[0] = 0
+                        
+    playerPos[1] += playerVel[1] * tickTime
+    cameraOffset[1] += playerVel[1] * tickTime
+    for row in world:
+            for tile in row:
+                if tile[0] != "TILE_AIR":
+                    if Rect(playerPos[0], playerPos[1], 45, 45).colliderect(Rect(tile[1][0] * 45, tile[1][1] * 45, 45, 45)):
+                        playerPos[1] -= playerVel[1] * tickTime
+                        cameraOffset[1] -= playerVel[1] * tickTime
+                        playerVel[1] = 0
+                        
     if keys[K_d]:
-        playerPos[0] += 135 * (time.time() - tickTime)
-        cameraOffset[0] += 135 * (time.time() - tickTime)
+        playerVel[0] += 665 * tickTime
+        
     if keys[K_a]:
-        playerPos[0] -= 135 * (time.time() - tickTime)
-        cameraOffset[0] -= 135 * (time.time() - tickTime)
-    if keys[K_w]:
-        playerPos[1] -= 135 * (time.time() - tickTime)
-        cameraOffset[1] -= 135 * (time.time() - tickTime)
-    if keys[K_s]:
-        playerPos[1] += 135 * (time.time() - tickTime)
-        cameraOffset[1] += 135 * (time.time() - tickTime)
+        playerVel[0] -= 665 * tickTime
+
+    if keys[K_SPACE] and playerVel[1] == 0:
+        playerVel[1] -= 665
+        
+    playerVel[1] += 665 * tickTime
+        
