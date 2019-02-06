3b82cc9aa163dcf7319e375962fb1573322d414f
Elliot Snethen <elliot.snethen@gmail.com>
2019-02-05 17:02:00 -0800

    made tree bigger

--- WorldGeneration.py
+++ WorldGeneration.py
@@ -20,7 +20,7 @@ def generateWorld():
             for tile in range(len(world[row])):
                 if world[row][tile][1] in [[x, y], [x, y - 1], [x, y - 2], [x, y - 3]]:
                     world[row][tile] = ["TILE_WOOD", world[row][tile][1], 1.5, 1.5]
-                if world[row][tile][1] in [[x - 1, y - 2], [x + 1, y - 2], [x - 1, y - 3], [x + 1, y - 3], [x, y - 4]]:
+                if world[row][tile][1] in [[x - 1, y - 2], [x + 1, y - 2], [x - 1, y - 3], [x + 1, y - 3], [x - 2, y - 2], [x + 2, y - 2], [x - 2, y - 3], [x + 2, y - 3], [x, y - 4], [x - 1, y - 4], [x + 1, y - 4]]:
                     world[row][tile] = ["TILE_LEAF", world[row][tile][1], 0.25, 0.25]
 
 
